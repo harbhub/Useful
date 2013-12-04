2db234b8b73c0f2e9d4368cc9fd56a0bd0d10e12
harbhub <harbhub@gmail.com>
2013-12-04 10:40:51 -0500

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,26 +1,29 @@
 'use strict';
-console.log('demo.js');
-//Create List Container
-var obj = {
-	tag: 'ul',
-	id: 'demo_list',
-	style: {
-		width: '200px',
-		height: '100px',
-		border: '1px solid black'
-	}
-};
-var el = Useful.NewElement(obj);
-Useful.AddElementToTop(el, document.body);
+console.log('demo.js Window Loading');
+window.onload = function () {
+	console.log('demo.js Window Loaded');
+	//Create List Container
+	var obj = {
+		tag: 'ul',
+		id: 'demo_list',
+		style: {
+			width: '200px',
+			height: '100px',
+			border: '1px solid black'
+		}
+	};
+	var el = Useful.NewElement(obj);
+	Useful.AddElementToTop(el, document.body);
+	
+	//Create List Item
+	var obj_item = {
+		tag: 'li',
+		style: {
+			height: '10px'
+		},
+		innerHTML: 'Hello'
+	};
+	var item = Useul.NewElement(obj_item);
+	Useful.AddElementToTop(el, document.getElementById('demo_list'));
 
-//Create List Item
-var obj_item = {
-	tag: 'li',
-	style: {
-		height: '10px'
-	},
-	innerHTML: 'Hello'
 };
-var item = Useul.NewElement(obj_item);
-Useful.AddElementToTop(el, document.getElementById('demo_list'));
-
