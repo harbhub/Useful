a6d2f8d7ac12ef0648773e7c73826d31b1f0af3f
harbhub <harbhub@gmail.com>
2013-12-04 10:35:19 -0500

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//Create List Container
+var obj = {
+	tag: 'ul',
+	id: 'demo_list',
+	style: {
+		width: '200px',
+		height: '100px',
+		border: '1px solid black'
+	}
+};
+var el = Useful.NewElement(obj);
+Useful.AddElementToTop(el, document.body);
+
+//Create List Item
+var obj_item = {
+	tag: 'li',
+	style: {
+		height: '10px'
+	},
+	innerHTML: 'Hello'
+};
+var item = Useul.NewElement(obj_item);
+Useful.AddElementToTop(el, document.getElementById('demo_list'));
+
